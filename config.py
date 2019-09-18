0d6752b5a07cd2c767fc07963aeecfb537e07e8d
joshua-vegas <vegas.joshua@gmail.com>
2019-09-18 14:30:07 +0200

    Modification du fichier init et config pour utiliser sqlalchemy et migrate

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ma_cle_secrete'
+
+    # Création d'une variable de configuration de connecter le package sqlAlchemy
+    # à notre base de données (DATABASE_URL).
+    # Si il n'y a pas de bdd, il créer une bdd sqlite qui se nomme app.db
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+# Objet représentant la base de données
+db = SQLAlchemy(app)
+# Objet représentant le moteur de migration
+migrate = Migrate(app, db)
 
 # On spécifie à flask, au moment de l'instanciation du projet, de lire et
 # d'appliquer le fichier de configuration config.py
 app.config.from_object(Config)
 
-from webapp import routes
+from webapp import routes, models
