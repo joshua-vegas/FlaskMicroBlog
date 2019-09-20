1d7d736bad9734bef41b32f4813d9fb0968e62ad
joshua-vegas <vegas.joshua@gmail.com>
2019-09-20 10:16:34 +0200

    Ajout du login dans le fichier init

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,6 +11,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # Objet représentant le moteur de migration
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
 
 # On spécifie à flask, au moment de l'instanciation du projet, de lire et
 # d'appliquer le fichier de configuration config.py
